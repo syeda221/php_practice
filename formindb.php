9d5b92c790a94a7cc28edca62cbe81efce8bff3f
qunoot zehra <qunootzehra21@gmail.com>
2025-12-30 18:19:34 +0500

    update

--- formindb.php
+++ formindb.php
@@ -13,13 +13,27 @@
     <form action="#" method="POST">
        First name: <input type="text" name="fname" id=""><br>
         last name:<input type="text" name="lname" id=""><br>
-<label for="gender">gender</label>
+<label for="gender">gender</label><br>
        female<input type="radio" value="female" name="gender" id="">
        male<input type="radio" value="male"name="gender" id=""><br>
       age: <input type="text" name="age" id=""><br>
-      email<input type="email" name="email" id="">
-
+      email<input type="email" name="email" id=""><br>
+      message:
+    <textarea name="message" id=""></textarea><br>
+    <input type="submit" value="submit">
 
     </form>
+
+    <?php 
+    if($_SERVER["REQUEST_METHOD"]== "POST"){
+        $fname = $_POST["fname"];
+        $lname = $_POST["lname"];
+        $gender = $_POST["gender"];
+        $age  = $_POST["age"];
+        $email  = $_POST["email"];
+        $message = $_POST["message"];
+
+    }
+    ?>
 </body>
 </html>
